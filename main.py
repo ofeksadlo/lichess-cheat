2c23734c86b6b56c825f71a5550aa3e84d0f92b4
ofeksadlo <55595248+ofeksadlo@users.noreply.github.com>
2020-09-05 19:57:59 +0300

    Printing out next best move

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
-import cv2
+import cv2, win32gui, win32con
 from pyautogui import screenshot
 import numpy as np
 from string import ascii_lowercase
-import win32gui
-import win32con
+from stockfish import Stockfish
+stockfish = Stockfish('C:\stockfish_20090216_x64_bmi2.exe')
 
-hwnd = win32gui.GetForegroundWindow()
-win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,100,100,200,200,0)
+# hwnd = win32gui.GetForegroundWindow()
+# win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,1600,300,200,200,0)
 
-# import os
-# os.system("mode con cols=50 lines=20")
 
-clientTurns = True
-# Check 2 images for missmatched pixels. The lower the number the better the match.
-# (This is a very simple method for image matching but efficent for our cause.)
-def matchImages(img1, img2):
-    err = np.sum((img1.astype("float") - img2.astype("float")) ** 2)
-    err /= float(img1.shape[0]*img2.shape[1])
-    return err
-
-# Using image matching we will check if it's the client turnImg.
-# Further more we will test if the client is on game to avoid failing.
 def checkTurn(turnImg):
     clientTurns = None
     clientTurns_template = cv2.imread('data/turn.png')
@@ -31,8 +19,17 @@ def checkTurn(turnImg):
         clientTurns = False
     return clientTurns
 
+# Check 2 images for missmatched pixels. The lower the number the better the match.
+# (This is a very simple method for image matching but efficent for our cause.)
+def matchImages(img1, img2):
+    err = np.sum((img1.astype("float") - img2.astype("float")) ** 2)
+    err /= float(img1.shape[0]*img2.shape[1])
+    return err
 # Gets board image and return last moveset.
 def getLastMove(board, cellSize=92):
+    e8LightGreen = False
+    h8DarkGreen = False
+    castling = False
     # Reading the cell templates later on we will match them to the board cells.
     movedFromCell_whiteTemplate = cv2.imread('data/white_from.jpg')
     movedFromCell_blackTemplate = cv2.imread('data/black_from.jpg')
@@ -60,20 +57,34 @@ def getLastMove(board, cellSize=92):
             # If the cell in the top left corner is green. And in the middle is green as well. Then
             # we found where the player moved from. Otherwise if the cell is green at the top left
             # but not green in the middle. Then it has to be where the player moved to.
+            #
+            #There is an image example in Examples folder
+
             if matchImages(board_cell[1:2, 1:2], movedFromCell_whiteTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_whiteTemplate[45:47,45:47])<10:
-                    lastMoveSet[0] = letters[y]+str(8-x)
+                if letters[y]+str(8-x) == 'e8':
+                    e8LightGreen = True
+                lastMoveSet[0] = letters[y]+str(8-x)
             elif matchImages(board_cell[1:2, 1:2], movedFromCell_blackTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_blackTemplate[45:47,45:47])<10:
-                    lastMoveSet[0] = letters[y]+str(8-x)
+                if letters[y]+str(8-x) == 'h8':
+                    h8DarkGreen = True
+                lastMoveSet[0] = letters[y]+str(8-x)
             elif matchImages(board_cell[1:2, 1:2], movedToCell_whiteTemplate[1:2,1:2]) < 10:
-                    lastMoveSet[1] = letters[y]+str(8-x)
+                lastMoveSet[1] = letters[y]+str(8-x)
             elif matchImages(board_cell[1:2, 1:2], movedToCell_blackTemplate[1:2,1:2]) < 10:
-                    lastMoveSet[1] = letters[y]+str(8-x)
+                lastMoveSet[1] = letters[y]+str(8-x)
             
+    if e8LightGreen == True and h8DarkGreen == True and castling == False:
+        castling = True
+        return ["e8", "g8"]
+
     if lastMoveSet[0] == '' or lastMoveSet[1] == '':
         return ''
+
     return lastMoveSet
 
-lastMove = ['','']
+gameMoveSet = []
+lastMove = ["",""]
+nextBestMove = None
 while True:
     # screenshot('turn.png', region=(1326, 670,78,16))
     # turnImg = cv2.imread('turn.png')
@@ -82,12 +93,20 @@ while True:
     if getLastMove(board) != '':
         if lastMove != getLastMove(board):
             lastMove = getLastMove(board)
-            print(lastMove)
-            # cv2.waitKey(1000)
+            gameMoveSet.append(lastMove[0] + lastMove[1])
+            stockfish.set_position(gameMoveSet)
+            print('Last moveset: ' + lastMove[0] + lastMove[1])
+            nextBestMove = stockfish.get_best_move()
+            print('Best next move: ' + nextBestMove)
     
     cv2.waitKey(1000)
+    # board = cv2.imread('board.png')
 
+    # letters = list(ascii_lowercase[:8])
+    # letters.index()
 
+    # board_show = cv2.resize(board,(0,0), fx=0.5, fy=0.5)
+    # cv2.imshow('Board', board_show)
     
     
 # board = cv2.imread('frame.png')
