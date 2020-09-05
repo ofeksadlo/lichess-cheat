39b4a65489428047787c5addc985a316c4043ee7
ofeksadlo <55595248+ofeksadlo@users.noreply.github.com>
2020-09-05 13:38:46 +0300

    Added back movedToCell_whiteTemplate movedToCell_blackTemplate
    Turns out the templates had different colors they are necessary for the color matching.

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import cv2
 from pyautogui import screenshot
 import numpy as np
 from string import ascii_lowercase
+import win32gui
+import win32con
 
+hwnd = win32gui.GetForegroundWindow()
+win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,100,100,200,200,0)
 
-import os
-os.system("mode con cols=50 lines=20")
+# import os
+# os.system("mode con cols=50 lines=20")
 
-
+clientTurns = True
 # Check 2 images for missmatched pixels. The lower the number the better the match.
 # (This is a very simple method for image matching but efficent for our cause.)
 def matchImages(img1, img2):
@@ -32,6 +36,8 @@ def getLastMove(board, cellSize=92):
     # Reading the cell templates later on we will match them to the board cells.
     movedFromCell_whiteTemplate = cv2.imread('data/white_from.jpg')
     movedFromCell_blackTemplate = cv2.imread('data/black_from.jpg')
+    movedToCell_whiteTemplate = cv2.imread('data/black_to.jpg')
+    movedToCell_blackTemplate = cv2.imread('data/white_to.jpg')
     # Creating a list with alphabet range a-h (8 letters total).
     letters = list(ascii_lowercase[:8])
     lastMoveSet = ['','']
@@ -48,32 +54,38 @@ def getLastMove(board, cellSize=92):
             # it will have a diffrent color, if it moved from a white piece.
             # If the cell is not piece that moved from. Then
             # we check if it's a piece that moved to this cell. Again for both templates white and black.
-            if matchImages(board_cell[1:2, 1:2], movedFromCell_whiteTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_whiteTemplate[45:47,45:47])<10:
-                lastMoveSet[0] = letters[y]+str(8-x)
-            elif matchImages(board_cell[1:2, 1:2], movedFromCell_blackTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_blackTemplate[45:47,45:47])<10:
-                lastMoveSet[0] = letters[y]+str(8-x)
-            elif matchImages(board_cell[1:2, 1:2], movedFromCell_whiteTemplate[1:2,1:2]) < 10:
-                lastMoveSet[1] = letters[y]+str(8-x)
-            elif matchImages(board_cell[1:2, 1:2], movedFromCell_blackTemplate[1:2,1:2]) < 10:
-                lastMoveSet[1] = letters[y]+str(8-x)
-
             
-    
+
+            # In this "long" if statements all we check for is matching colors.
+            # If the cell in the top left corner is green. And in the middle is green as well. Then
+            # we found where the player moved from. Otherwise if the cell is green at the top left
+            # but not green in the middle. Then it has to be where the player moved to.
+            if matchImages(board_cell[1:2, 1:2], movedFromCell_whiteTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_whiteTemplate[45:47,45:47])<10:
+                    lastMoveSet[0] = letters[y]+str(8-x)
+            elif matchImages(board_cell[1:2, 1:2], movedFromCell_blackTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_blackTemplate[45:47,45:47])<10:
+                    lastMoveSet[0] = letters[y]+str(8-x)
+            elif matchImages(board_cell[1:2, 1:2], movedToCell_whiteTemplate[1:2,1:2]) < 10:
+                    lastMoveSet[1] = letters[y]+str(8-x)
+            elif matchImages(board_cell[1:2, 1:2], movedToCell_blackTemplate[1:2,1:2]) < 10:
+                    lastMoveSet[1] = letters[y]+str(8-x)
+            
+    if lastMoveSet[0] == '' or lastMoveSet[1] == '':
+        return ''
     return lastMoveSet
 
 lastMove = ['','']
 while True:
-    screenshot('turn.png', region=(1326, 670,78,16))
-    turnImg = cv2.imread('turn.png')
-    if checkTurn(turnImg) == True:
-        screenshot('board.png', region=(575,164,735,735))
-        board = cv2.imread('board.png')
-        if getLastMove(board) != '':
-            if lastMove != getLastMove(board):
-                lastMove = getLastMove(board)
-                print(lastMove)
-        # cv2.waitKey(1000)
-    cv2.waitKey(500)
+    # screenshot('turn.png', region=(1326, 670,78,16))
+    # turnImg = cv2.imread('turn.png')
+    screenshot('board.png', region=(575,164,735,735))
+    board = cv2.imread('board.png')
+    if getLastMove(board) != '':
+        if lastMove != getLastMove(board):
+            lastMove = getLastMove(board)
+            print(lastMove)
+            # cv2.waitKey(1000)
+    
+    cv2.waitKey(1000)
 
 
     
