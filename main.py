a9d800ef1692c1d75dd0d62b86561db797036b82
ofeksadlo <55595248+ofeksadlo@users.noreply.github.com>
2020-09-08 00:28:14 +0300

    Added support for both color player

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ stockfish.set_depth(16)
 cv2.namedWindow('Board')
 # Prioritizing the board window over other windows
 hwnd = win32gui.GetForegroundWindow()
+# Positining the board window change the values if you don't see it show up.
 win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,-16,150,0,0,0)
 
 # This function checks if it's the client turn or the opponent turn
@@ -43,7 +44,7 @@ def matchImages(img1, img2):
 # Using this function we highlight the best moves possible
 # The function gets the board and the moveset we want to highlight
 # And returns the board with the highlighted cells
-def drawOnBoard(board, moveset, cellSize=92):
+def drawOnBoard(board, moveset,whitePlayer, cellSize=92):
 
     letters = list(ascii_lowercase[:8])
 
@@ -58,20 +59,30 @@ def drawOnBoard(board, moveset, cellSize=92):
     for y in range(8):
         for x in range(8):
             board_cell = board[x * cellSize:(x+1) * cellSize, y * cellSize:(y+1) * cellSize]
-            if letters[y]+str(8-x) == cell1:
-                painted_board = cv2.rectangle(painted_board, (y*cellSize, x*cellSize), ((y+1)*cellSize, (x+1)*cellSize), (255,0,0), thickness=2)
-            elif  letters[y]+str(8-x) == cell2:
-                painted_board = cv2.rectangle(painted_board, (y*cellSize, x*cellSize), ((y+1)*cellSize, (x+1)*cellSize), (255,0,0), thickness=2)
+            if whitePlayer == True:
+                if letters[y]+str(8-x) == cell1:
+                    painted_board = cv2.rectangle(painted_board, (y*cellSize, x*cellSize), ((y+1)*cellSize, (x+1)*cellSize), (255,0,0), thickness=2)
+                elif  letters[y]+str(8-x) == cell2:
+                    painted_board = cv2.rectangle(painted_board, (y*cellSize, x*cellSize), ((y+1)*cellSize, (x+1)*cellSize), (255,0,0), thickness=2)
+            elif whitePlayer == False:
+                reversed_letters = letters[::-1]
+                if reversed_letters[y]+str(x+1) == cell1:
+                    painted_board = cv2.rectangle(painted_board, (y*cellSize, x*cellSize), ((y+1)*cellSize, (x+1)*cellSize), (255,0,0), thickness=2)
+                elif  reversed_letters[y]+str(x+1) == cell2:
+                    painted_board = cv2.rectangle(painted_board, (y*cellSize, x*cellSize), ((y+1)*cellSize, (x+1)*cellSize), (255,0,0), thickness=2)
 
     return painted_board
 
 
 
 # Gets board image and return last moveset.
-def getLastMove(board, cellSize=92):
+def getLastMove(board, whitePlayer, cellSize=92):
     e8LightGreen = False
     h8DarkGreen = False
     a8LightGreen = False
+    h1LightGreen = False
+    a1DarkGreen = False
+    e1DarkGreen = False
     castling = False
     # Reading the cell templates later on we will match them to the board cells.
     movedFromCell_whiteTemplate = cv2.imread('data/white_from.dat')
@@ -104,20 +115,43 @@ def getLastMove(board, cellSize=92):
             # Further more we check for possible castling that is why we check for cells e8, a8, h8.
             # Because this are the possible castling cells of the opponent and are method won't work.
             # Because we will have in this moveset(for example e8g8) 2 empty green cells.
-            if matchImages(board_cell[1:2, 1:2], movedFromCell_whiteTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_whiteTemplate[45:47,45:47])<10:
-                if letters[y]+str(8-x) == 'e8':
-                    e8LightGreen = True
-                elif letters[y]+str(8-x) == 'a8':
-                    a8LightGreen = True
-                lastMoveSet[0] = letters[y]+str(8-x)
-            elif matchImages(board_cell[1:2, 1:2], movedFromCell_blackTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_blackTemplate[45:47,45:47])<10:
-                if letters[y]+str(8-x) == 'h8':
-                    h8DarkGreen = True
-                lastMoveSet[0] = letters[y]+str(8-x)
-            elif matchImages(board_cell[1:2, 1:2], movedToCell_whiteTemplate[1:2,1:2]) < 10:
-                lastMoveSet[1] = letters[y]+str(8-x)
-            elif matchImages(board_cell[1:2, 1:2], movedToCell_blackTemplate[1:2,1:2]) < 10:
-                lastMoveSet[1] = letters[y]+str(8-x)
+            if whitePlayer == True:
+                if matchImages(board_cell[1:2, 1:2], movedFromCell_whiteTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_whiteTemplate[45:47,45:47])<10:
+                    if letters[y]+str(8-x) == 'e8':
+                        e8LightGreen = True
+                    elif letters[y]+str(8-x) == 'a8':
+                        a8LightGreen = True
+                    lastMoveSet[0] = letters[y]+str(8-x)
+                elif matchImages(board_cell[1:2, 1:2], movedFromCell_blackTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_blackTemplate[45:47,45:47])<10:
+                    if letters[y]+str(8-x) == 'h8':
+                        h8DarkGreen = True
+                    lastMoveSet[0] = letters[y]+str(8-x)
+                elif matchImages(board_cell[1:2, 1:2], movedToCell_whiteTemplate[1:2,1:2]) < 10:
+                    lastMoveSet[1] = letters[y]+str(8-x)
+                elif matchImages(board_cell[1:2, 1:2], movedToCell_blackTemplate[1:2,1:2]) < 10:
+                    lastMoveSet[1] = letters[y]+str(8-x)
+            elif whitePlayer == False:
+                reversed_letters = letters[::-1]
+                if matchImages(board_cell[1:2, 1:2], movedFromCell_whiteTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_whiteTemplate[45:47,45:47])<10:
+                    if reversed_letters[y]+str(x+1) == 'e8':
+                        e8LightGreen = True
+                    elif reversed_letters[y]+str(x+1) == 'a8':
+                        a8LightGreen = True
+                    elif reversed_letters[y]+str(x+1) == 'h1':
+                        h1LightGreen = True
+                    lastMoveSet[0] = reversed_letters[y]+str(x+1)
+                elif matchImages(board_cell[1:2, 1:2], movedFromCell_blackTemplate[1:2,1:2]) < 10 and matchImages(board_cell[45:47,45:47], movedFromCell_blackTemplate[45:47,45:47])<10:
+                    if reversed_letters[y]+str(x+1) == 'e1':
+                        e1DarkGreen = True
+                    elif reversed_letters[y]+str(x+1) == 'a1':
+                        a1DarkGreen = True
+                    elif letters[y]+str(8-x) == 'h8':
+                        h8DarkGreen = True
+                    lastMoveSet[0] = reversed_letters[y]+str(x+1)
+                elif matchImages(board_cell[1:2, 1:2], movedToCell_whiteTemplate[1:2,1:2]) < 10:
+                    lastMoveSet[1] = reversed_letters[y]+str(x+1)
+                elif matchImages(board_cell[1:2, 1:2], movedToCell_blackTemplate[1:2,1:2]) < 10:
+                    lastMoveSet[1] = reversed_letters[y]+str(x+1)
     # We check if it is a castling
     if e8LightGreen == True and h8DarkGreen == True and castling == False:
         castling = True
@@ -125,6 +159,11 @@ def getLastMove(board, cellSize=92):
     elif e8LightGreen == True and a8LightGreen == True and castling == False:
         castling = True
         return ["e8", "c8"]
+    elif e1DarkGreen == True and h1LightGreen == True:
+        castling = True
+        return ["e1", "g1"]
+    elif e1DarkGreen == True and a1DarkGreen == True:
+        return ["e1", "c1"]
     # If nothing moved we return ''
     if lastMoveSet[0] == '' or lastMoveSet[1] == '':
         return ''
@@ -153,6 +192,10 @@ nextBestMove = None
 f= open('logs/logFile.txt', 'a')
 # Deleting the previous game moves.
 f.write('')
+
+playerColor = input('Insert your pieces color (\'b\' = black / \'w\' = white): ')
+if playerColor == 'b':
+    clientTurns = False
 while True:
     # We snap which turn is it.
     screenshot('turn.png', region=(1335, 665,10,16))
@@ -169,10 +212,10 @@ while True:
         cv2.waitKey(250)
         screenshot('board.png', region=(575,164,735,735))
         board = cv2.imread('board.png')
-        if getLastMove(board) != '':
-            nextLastMove = getLastMove(board)
+        if getLastMove(board, (playerColor == 'w')) != '':
+            nextLastMove = getLastMove(board, (playerColor == 'w'))
             if (lastMove[0] + lastMove[1]) != (nextLastMove[0] + nextLastMove[1]):
-                lastMove = getLastMove(board)
+                lastMove = getLastMove(board, (playerColor == 'w'))
                 gameMoveSet.append(lastMove[0] + lastMove[1])
                 stockfish.set_position(gameMoveSet)
                 print('Last moveset: ' + lastMove[0] + lastMove[1])
@@ -183,7 +226,7 @@ while True:
     
     if nextBestMove is not None:
         board_show = cv2.imread('board.png')
-        board_show = drawOnBoard(board_show, nextBestMove)
+        board_show = drawOnBoard(board_show, nextBestMove, (playerColor == 'w'))
         board_show = cv2.resize(board_show, None, fx=0.785, fy=0.785)
         cv2.imshow('Board', board_show)
 
